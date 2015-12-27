095831b586d33d1b5de6964e199f963c9b20e8e7
Łukasz Adler <ckmkingkong@gmail.com>
2015-12-27 18:52:17 +0100

    Remove SharpRepositories references

--- SpeedwayCenter/SpeedwayCenter/App_Start/NinjectWebCommon.cs
+++ SpeedwayCenter/SpeedwayCenter/App_Start/NinjectWebCommon.cs
@@ -1,7 +1,5 @@
 using System.Configuration;
 using System.Data.Entity;
-using SharpRepository.Ioc.Ninject;
-using SharpRepository.Repository.Ioc;
 using SpeedwayCenter.Models.Entity_Framework;
 
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(SpeedwayCenter.App_Start.NinjectWebCommon), "Start")]
@@ -67,13 +65,7 @@ namespace SpeedwayCenter.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            kernel.BindSharpRepository();
-            RepositoryDependencyResolver.SetDependencyResolver(new NinjectDependencyResolver(kernel));
-            kernel.Bind<DbContext>()
-                .To<SpeedwayEntities>()
-                .InRequestScope()
-                .WithConstructorArgument("connectionString",
-                    ConfigurationManager.ConnectionStrings["SpeedwayEntities"].ConnectionString);
+            
         }        
     }
 }
