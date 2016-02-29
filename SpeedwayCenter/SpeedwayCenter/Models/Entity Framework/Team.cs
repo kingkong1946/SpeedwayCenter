956a695b107f73c9e2608370d489e232409048cb
Łukasz Adler <ckmkingkong@gmail.com>
2016-02-29 10:53:31 +0100

    -addes some models

---
+++ SpeedwayCenter/SpeedwayCenter/Models/Entity Framework/Team.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpeedwayCenter.Models.Entity_Framework
+{
+    public class Team
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Column(TypeName = "nvarchar")]
+        [RegularExpression("^[A-Za-z ]+", ErrorMessage = "Invalid characters")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Column(TypeName = "nvarchar")]
+        [RegularExpression("^[A-Za-z ]+", ErrorMessage = "Invalid characters")]
+        public string City { get; set; }
+        
+        public virtual ICollection<Meeting> HomeMeetings { get; set; }
+        public virtual ICollection<Meeting> AwayMeetings { get; set; }
+
+        public virtual ICollection<Rider> Riders { get; set; }
+    }
+}
